ed58e05a31aba2bfc3ba53cb228739ea6ce1a49c
saint6839 <saint6839@gmail.com>
2024-06-28 20:40:28 +0900

    feat: 강의 조회 usecase 주석 추가

--- src/lecture/usecase/browse-lectures.usecase.ts
+++ src/lecture/usecase/browse-lectures.usecase.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { Lecture } from "../domain/entity/lecture";
+import { LectureDetail } from "../domain/entity/lecture-detail";
 import { ILectureDetailRepository } from "../domain/interface/repository/lecture-deteail.repository.interface";
 import { ILectureRepository } from "../domain/interface/repository/lecture.repository.interface";
 import { IBrowseLecturesUseCase } from "../domain/interface/usecase/browse-lectures.usecase.interface";
@@ -20,6 +21,10 @@ export class BrowseLecturesUseCase implements IBrowseLecturesUseCase {
     private readonly lectureDetailMapper: LectureDetailMapper
   ) {}
 
+  /**
+   * @description 모든 강의 목록과 해당 강의의 상세 일정을 모두 조회하는 usecase
+   * @returns LectureDto[]
+   */
   async execute(): Promise<LectureDto[]> {
     const lectures = await this.fetchAllLectures();
     return await Promise.all(
@@ -30,7 +35,11 @@ export class BrowseLecturesUseCase implements IBrowseLecturesUseCase {
     );
   }
 
-  private async fetchAllLectures() {
+  /**
+   * @description 모든 강의 목록을 조회하는 함수
+   * @returns Lecture[]
+   */
+  private async fetchAllLectures(): Promise<Lecture[]> {
     const lectureEntities = await this.lectureRepository.findAll();
     const lectures = lectureEntities.map((entity) =>
       this.lectureMapper.toDomainFromEntity(entity)
@@ -38,7 +47,14 @@ export class BrowseLecturesUseCase implements IBrowseLecturesUseCase {
     return lectures;
   }
 
-  private async fetchLectureDetails(lecture: Lecture) {
+  /**
+   * @description 특정 강의의 상세 일정을 조회하는 함수
+   * @param lecture
+   * @returns LectureDetail[]
+   */
+  private async fetchLectureDetails(
+    lecture: Lecture
+  ): Promise<LectureDetail[]> {
     const lectureDetailEntities =
       await this.lectureDetailRepository.findByLectureId(lecture.id);
     const lectureDetails = lectureDetailEntities.map((entity) =>
