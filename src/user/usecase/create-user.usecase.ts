9fac421becf31c9ffb297fde8d6dd15cfba8b4a8
saint6839 <saint6839@gmail.com>
2024-06-28 20:41:06 +0900

    docs: 사용자 생성 usecase 주석 추가

--- src/user/usecase/create-user.usecase.ts
+++ src/user/usecase/create-user.usecase.ts
@@ -1,12 +1,11 @@
-import { UserEntity } from "src/user/infrastructure/entity/user.entity";
 import { Inject, Injectable } from "@nestjs/common";
-import { UserRepository } from "../infrastructure/repository/user.repository";
-import { ICreateUserUseCase } from "../domain/interface/usecase/create-user.usecase.interface";
-import { CreateUserDto } from "../presentation/dto/request/create-user.dto";
-import { UserDto } from "../presentation/dto/response/user.dto";
-import { UserMapper } from "../domain/mapper/user.mapper";
 import { User } from "../domain/entity/user";
 import { IUserRepository } from "../domain/interface/repository/user.repository.interface";
+import { ICreateUserUseCase } from "../domain/interface/usecase/create-user.usecase.interface";
+import { UserMapper } from "../domain/mapper/user.mapper";
+import { UserRepository } from "../infrastructure/repository/user.repository";
+import { CreateUserDto } from "../presentation/dto/request/create-user.dto";
+import { UserDto } from "../presentation/dto/response/user.dto";
 
 @Injectable()
 export class CreateUserUseCase implements ICreateUserUseCase {
@@ -16,6 +15,11 @@ export class CreateUserUseCase implements ICreateUserUseCase {
     private readonly userMapper: UserMapper
   ) {}
 
+  /**
+   * @description 사용자 생성 usecase
+   * @param dto
+   * @returns UserDto
+   */
   async execute(dto: CreateUserDto): Promise<UserDto> {
     const user = User.create(dto.name);
     const userEntity = await this.userRepository.create(user);
